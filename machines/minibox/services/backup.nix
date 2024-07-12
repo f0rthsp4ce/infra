d9a26a789bf070dc5176615cb265587906af93b8
cofob <cofob@riseup.net>
2024-07-13 00:44:46 +0400

    Fix key and env in proxmox backup

--- machines/minibox/services/backup.nix
+++ machines/minibox/services/backup.nix
@@ -5,14 +5,13 @@
     "${self}/secrets/credentials/proxmox-backup/key.age";
   age.secrets.credentials-proxmox-backup-env.file =
     "${self}/secrets/credentials/proxmox-backup/env.age";
-
   services.proxmox-backup = {
     enable = true;
     fingerprint =
       "b6:58:13:55:b5:73:b4:ff:8f:3c:f0:c6:2e:ee:0d:3c:dc:41:18:c8:6f:2b:6b:32:f3:3a:0c:23:09:f6:05:b9";
     namespace = "f0rthsp4ce";
-    keyFile = config.age.secrets.credentials-proxmox-backup-key.file;
-    envFile = config.age.secrets.credentials-proxmox-backup-env.file;
+    keyFile = config.age.secrets.credentials-proxmox-backup-key.path;
+    envFile = config.age.secrets.credentials-proxmox-backup-env.path;
   };
 
   age.secrets.credentials-telegram-backup.file =
