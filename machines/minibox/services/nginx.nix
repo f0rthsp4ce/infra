91c446239187849f160d06ccd55db74a6b4db951
Egor Ternovoi <cofob@riseup.net>
2025-08-13 04:37:23 +0400

    Fix

--- machines/minibox/services/nginx.nix
+++ machines/minibox/services/nginx.nix
@@ -114,7 +114,7 @@ in {
           proxy_set_header        Host $host;
           proxy_set_header        X-Real-IP $remote_addr;
           # Home Assistant doesn't work correctly with the X-Forwarded-Proto header
-          # proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header        X-Forwarded-Proto $scheme;
           proxy_set_header        X-Forwarded-Host $host;
           proxy_set_header        X-Forwarded-Server $host;
